e1a983bbd7badcb5174e370ec4e32297f08afab5
Dhanashri Gadave (MINDTREE LIMITED) <v-dgadave@microsoft.com>
2023-08-25 20:24:29 +0530

    add commit1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors')
+const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
 
