26cc7f55a4eaaab09a16b7ce1299568eee05fc56
Dhanashri Gadave (MINDTREE LIMITED) <v-dgadave@microsoft.com>
2023-08-28 13:21:00 +0530

    port add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pkg = require('./package.json');
 
 
 // App constants
-const port = 3000;
+const port = process.env.PORT || 3000;
 const apiPrefix = '/api';
 
 // Store data in-memory, not suited for production use!
